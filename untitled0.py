252575812028e0f0661122785515a4d6b92aafc1
aaditya5555 <aadityasalwan55@gmail.com>
2025-10-17 03:03:48 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,6 @@
+#personal introduction
+name=input("enter your name")
+age=input("enter your age")
+hobby=input("enter your hobby")
+
+print(f"Welcome {name} , Your age is {age} and your hobby is {hobby}") 
